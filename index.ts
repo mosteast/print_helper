69b4c93657ff245ed8b8f44417fb44375c89c5ab
kev <kev@localhost>
2020-02-09 08:33:30 +0800

    Auto commit: 2020-02-09_08:33:30

---
+++ index.ts
@@ -0,0 +1,9 @@
+export {
+  print,
+  print_warn,
+  print_error,
+  print_verbose,
+  print_success,
+  print_info,
+} from './src/printer'
+
--- test/test.ts
+++
@@ -1,5 +0,0 @@
-import { yo } from '../src/printer'
-
-test('Can yo', () => {
-  expect(yo).toBe('yoo')
-})
